13e364493b223003b55c261fca8c7b6a207e5bcb
silberpavel <silber.ypm@gmail.com>
2018-10-16 18:02:46 +0300

    setInterval

--- slider_jquery.js
+++ slider_jquery.js
@@ -15,10 +15,41 @@
 
 var offset = 0;
 var count = $(".slide-item-wrapper > * > *").length;
+
+
+var i = -200;
 window.setInterval(
+
   function() {
-    offset = (offset - 200) % (count * 200); // 104px div height (incl margin)
+    // offset = (offset - 200) % (count * 200); // 104px div height (incl margin)
+    // offset = -200;
+
+    // for (var i = 0; i <= -800; i-200) {
+    
+
     $(".slide-item-wrapper > *").css({
-      "transform": "translateY(" + offset + "px)",
+      "transform": "translateY(" + i + "px)"
+      
     });
-  }, 5000);
+
+    if(i < -800) {
+    	i = -200;
+    }
+    
+
+	// }
+  }, 1000);
+
+    
+
+
+
+
+
+// $('.user-text').css({
+//   '-webkit-transform' : 'scale(' + ui.value + ')',
+//   '-moz-transform'    : 'scale(' + ui.value + ')',
+//   '-ms-transform'     : 'scale(' + ui.value + ')',
+//   '-o-transform'      : 'scale(' + ui.value + ')',
+//   'transform'         : 'scale(' + ui.value + ')'
+// });
